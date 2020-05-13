0839c0f01514398931fdc10b09ed91961a89f444
Carolene Wilson <carolenesw@gmail.com>
2020-05-13 15:25:53 -0400

    add required variables

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+
+// Create required variables based on quiz criteria:
+var question = quiQuestions.length - 1;
+var startEl = document.getElementById("start");
+var timerEl = document.getElementById("count");
+var scoreEl = document.getElementById("score");
+var intialEl = document.getElementById("initial");
+var submitEl = document.getElementById("submit"); 
+
